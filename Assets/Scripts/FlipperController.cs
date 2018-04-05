645e63a8aeb4b0c96e9a66b0c421ac874c581d33
Yamamoto Masao <yamamoto.masa-y@jp.fujitsu.com>
2018-04-05 16:16:40 +0900

    Lesson5_課題

---
+++ Assets/Scripts/FlipperController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperController : MonoBehaviour {
+	// HingeJointコンポーネントを入れる
+
+	// HigeJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+
+	// 初期の傾き
+	private float defaultAngle = 20;
+	// 弾いた時の傾き
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		// HingeJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		// フリッパーの傾きを設定
+		SetAngle(this.defaultAngle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// 左矢印方向キーを押したとき左手フリッパーを動かす
+		if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFlipperTag") {
+			SetAngle (this.flickAngle);
+		}
+		// 右矢印方向キーを押したとき右手フリッパーを動かす
+		if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFlipperTag") {
+			SetAngle (this.flickAngle);
+		}
+
+		// 矢印方向キーを離した時フリッパーを戻す
+		if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFlipperTag") {
+			SetAngle (this.defaultAngle);
+		}
+		if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFlipperTag") {
+			SetAngle (this.defaultAngle);
+		}
+			
+//		Touch[] myTouches = Input.touches;
+		for(int i = 0; i < Input.touchCount; i++)
+		{
+			Touch t = Input.GetTouch(i);
+//			var id = Input.touches[i].fingerId;
+			var pos = Input.touches[i].position;
+			if (t.phase == TouchPhase.Began) {
+				// 左画面をタッチしたとき左手フリッパーを動かす
+				if(pos.x < Screen.width / 2 && tag == "LeftFlipperTag") {
+					// 画面中央より左がタッチされたときの処理
+					SetAngle (this.flickAngle);
+				}
+				// 右画面をタッチしたとき右手フリッパーを動かす
+				if(pos.x > Screen.width / 2 && tag == "RightFlipperTag") {
+					SetAngle (this.flickAngle);
+				}
+			}
+			else {
+				if (t.phase == TouchPhase.Ended) {
+					// 左画面から指を離したとき左手フリッパーを戻す
+					if(pos.x < Screen.width / 2 && tag == "LeftFlipperTag") {
+						// 画面中央より左がタッチされたときの処理
+						SetAngle (this.defaultAngle);
+					}
+					// 右画面から指を離したとき右手フリッパーを戻す
+					if(pos.x > Screen.width / 2 && tag == "RightFlipperTag") {
+						SetAngle (this.defaultAngle);
+					}
+				}
+			}
+		}
+
+		// 矢印方向キーを離した時フリッパーを戻す
+		if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFlipperTag") {
+			SetAngle (this.defaultAngle);
+		}
+		if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFlipperTag") {
+			SetAngle (this.defaultAngle);
+		}
+
+	}
+
+	// フリッパーの傾きを設定
+	public void SetAngle (float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+}
